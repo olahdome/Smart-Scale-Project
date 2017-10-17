6dc606c92e5994b473c18b247b3d81d40262874d
Olah Dome <olah.dome@gmail.com>
2017-10-17 22:48:58 +0100

    Created new files for set up the bluetooth connection.

---
+++ Merleg_jo/bluetooth_connection.c
@@ -0,0 +1,6 @@
+/*
+ * bluetooth_connection.c
+ *
+ * Created: 2017.10.17. 21:11:24
+ *  Author: Dome
+ */ 
---
+++ Merleg_jo/bluetooth_connection.h
@@ -0,0 +1,16 @@
+/*
+ * bluetooth_connection.h
+ *
+ * Created: 2017.10.17. 21:11:39
+ *  Author: Dome
+ */ 
+
+
+#ifndef BLUETOOTH_CONNECTION_H_
+#define BLUETOOTH_CONNECTION_H_
+
+
+
+
+
+#endif /* BLUETOOTH_CONNECTION_H_ */
